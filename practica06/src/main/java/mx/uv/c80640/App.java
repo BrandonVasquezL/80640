90aa7e367bedc0cdb82d5b6989bb9711ee763f50
BrandonVasquezL <zs20022115@estudiantes.uv.mx>
2022-10-20 17:59:06 -0500

    practica06

--- practica06/src/main/java/mx/uv/c80640/App.java
+++ practica06/src/main/java/mx/uv/c80640/App.java
@@ -1,4 +1,5 @@
 package mx.uv.c80640;
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -9,5 +10,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello World");
+        get("/Eduardo", (req, res) -> "Hello Eduardo");
+        get("/Brandon", (req, res) -> "Hello Brandon");
+        get("/", (req, res) -> "<h1>Bienvenido</h1><img src='https://www.uv.mx/v2/images/logouv.jpg'>");
+    
+        post("/", (req, res)->{
+            System.out.println(req.queryParams("email")+ " "+ "" );
+            System.out.println();
+            return oRespuesta;
+        });
     }
 }

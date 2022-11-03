fdb74bb9f9bc48d09e4661e0d2ddcbc471429af7
BrandonVasquezL <zs20022115@estudiantes.uv.mx>
2022-11-03 12:50:11 -0600

    3/11/22

--- practica06/src/main/java/mx/uv/c80640/App.java
+++ practica06/src/main/java/mx/uv/c80640/App.java
@@ -1,6 +1,10 @@
 package mx.uv.c80640;
 import static spark.Spark.*;
 
+import org.eclipse.jetty.websocket.api.StatusCode;
+
+import com.google.gson.JsonObject;
+
 /**
  * Hello world!
  *
@@ -16,8 +20,12 @@ public class App
         get("/", (req, res) -> "<h1>Bienvenido</h1><img src='https://www.uv.mx/v2/images/logouv.jpg'>");
     
         post("/", (req, res)->{
-            System.out.println(req.queryParams("email")+ " "+ "" );
-            System.out.println();
+            System.out.println(req.queryParams("email")+ " "+ req.queryParams("password"));
+            System.out.println(req.body());
+            res.status(200);
+            JsonObject oRespuesta = new JsonObject();
+            oRespuesta.addProperty("msj", "Hola");
+            oRespuesta.addProperty("email", req.queryParams("email"));
             return oRespuesta;
         });
     }

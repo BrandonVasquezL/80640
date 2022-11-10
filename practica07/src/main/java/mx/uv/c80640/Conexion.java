a8af76a3e255de45612300885b8f906f77fe550c
BrandonVasquezL <zs20022115@estudiantes.uv.mx>
2022-11-10 13:04:36 -0600

    base de datos

--- practica07/src/main/java/mx/uv/c80640/Conexion.java
+++ practica07/src/main/java/mx/uv/c80640/Conexion.java
@@ -1,5 +1,8 @@
 package mx.uv.c80640;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import com.mysql.jdbc.Connection;
 
 public class Conexion{
@@ -9,4 +12,15 @@ public class Conexion{
     private static String password = "12345678";
     private static Connection connection = null;
 
+    public static Connection geConnection(){
+        try{
+            Class.forName(DriverName);
+            connection = DriverManager.getConnection(url, username, password).newInstance();
+        }catch(SQLException e){
+            System.out.println(e);
+        }catch(ClassNotFoundException e){
+            System.out.println("no encontro el driver");
+        }
+        return connection;
+    }
 }
